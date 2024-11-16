e352b3db34127dae6f96871177f9260afdd94936
avern2024 <avern2020@icloud.com>
2024-11-16 11:39:04 +0700

    Add files via upload

---
+++ module_7_2.py
@@ -0,0 +1,24 @@
+def custom_write(file_name, strings):
+    strings_positions = {}
+
+    with open(file_name, 'w', encoding='utf-8') as f:
+        for index, string in enumerate(strings, start=1):
+            bite_position = f.tell()
+            f.write(string + '\n')
+            strings_positions[(index,bite_position)] = string
+
+    return strings_positions
+
+
+if __name__ == '__main__':
+    # Пример выполнения кода
+    info = [
+        'Text for tell.',
+        'Используйте кодировку utf-8.',
+        'Because there are 2 languages!',
+        'Спасибо!'
+    ]
+
+    result = custom_write('test.txt', info)
+    for elem in result.items():
+        print(elem)
