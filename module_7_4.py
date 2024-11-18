30a27ceb8a56a1eab3d237eb3941d8dd24e13379
avern2024 <avern2020@icloud.com>
2024-11-18 10:22:53 +0700

    Add files via upload

---
+++ module_7_4.py
@@ -0,0 +1,24 @@
+# Использование %:
+team1_num = 5
+team2_num = 6
+print('В команде Мастера кода участников: %s!' % team1_num)
+print('В команде Мастера кода участников: %s и %s' % (team1_num, team2_num))
+print()
+
+# Использование format():
+score_1 = 40
+score_2 = 42
+team1_time = 18015.2
+team2_time = 2153.31451
+print('Команда Волшебники данных решила задач: {}!'.format(score_2))
+print('Волшебники данных решили задачи за {} c!'.format(team1_time))
+print('Волшебники данных решили задачи за {} c!'.format(team2_time))
+print()
+
+# Использование f-строк:
+challenge_result = 'Победа команды Волшебники данных!'
+tasks_total = 82
+time_avg = 45.2
+print(f'Команды решили {score_1} и {score_2} задач.')
+print(f'Результат битвы: {challenge_result}')
+print(f'Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу!')
